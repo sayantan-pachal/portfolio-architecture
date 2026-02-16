9eee93c38c4a542b7d2c990b2d37203db4fcc54a
Sayantan Pachal <sayantanpachal006@gmail.com>
2026-02-16 20:11:38 +0530

    update

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import UIFramework from "ui-library";
 
-function Contact() {
+// Reusable contact UI blocks
+import SocialGrid from "components/SocialGrid";
+import MessageForm from "components/MessageForm";
+
+// Contact-related data source
+import contactList from "data/contactList";
+
+export default function ContactPage() {
   return (
-    <div>Contact</div>
-  )
-}
+    <section className="page-wrapper">
+      <header className="page-header">
+        <h1>Let’s work together</h1>
+        <p><em>How do you take your coffee?</em></p>
+      </header>
 
-export default Contact
+      {/* Social / external contact links */}
+      <div className="contact-grid">
+        {contactList.map((item) => (
+          <SocialGrid
+            key={item.id}
+            platform={item.platform}
+            link={item.url}
+            handle={item.handle}
+            label={item.label}
+            icon={item.icon}
+          />
+        ))}
+      </div>
+
+      {/* Direct contact form */}
+      <section className="contact-form-section">
+        <MessageForm />
+      </section>
+    </section>
+  );
+}
