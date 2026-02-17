36b9b6f57bfcf5099007456ae4de5c5e56a165e7
Sayantan Pachal <sayantanpachal006@gmail.com>
2026-02-17 18:23:35 +0530

    update

---
+++ src/Components/Home/Parts/HomeWork.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function HomeWork() {
+  return (
+    // HomeWork(Projects) section, showing various work-related content related to the website's theme...
+    <div>HomeWork</div>
+  )
+}
+
+export default HomeWork
