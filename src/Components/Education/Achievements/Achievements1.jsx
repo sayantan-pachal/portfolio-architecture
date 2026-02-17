31abc9ee2f2521a56df195d21a249f9228a7413d
Sayantan Pachal <sayantanpachal006@gmail.com>
2026-02-17 18:17:01 +0530

    update

--- src/Components/Education/Achievements/Achievements1.jsx
+++ src/Components/Education/Achievements/Achievements1.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 function Achievements1() {
   return (
+    // Right side of the Achievements section, showing achievements icon right side of the page...
+    // Showing additional data of the achievements on left side of the page...
     <div>Achievements1</div>
   )
 }
--- src/Components/Education/Achievements/Achievements2.jsx
+++ src/Components/Education/Achievements/Achievements2.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 function Achievements2() {
   return (
+    // Left side of the Achievements section, showing achievements icon left side of the page...
+    // Showing additional data of the achievements on right side of the page...
     <div>Achievements2</div>
   )
 }
