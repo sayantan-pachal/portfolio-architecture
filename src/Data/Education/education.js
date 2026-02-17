4259196ec22d09638d20c3ba2d3ae9b20b6bfe21
Sayantan Pachal <sayantanpachal006@gmail.com>
2026-02-17 18:04:32 +0530

    update

---
+++ src/Data/Education/education.js
@@ -0,0 +1,31 @@
+const educations = [
+    {
+        id: 1,
+        institution: "B. P. Poddar Institute of Management and Technology (BPPIMT) - MAKAUT",
+        deg: "Bachelor of Technology(B.Tech)",
+        branch: "Computer Science & Engineering",
+        year: "2025-Present",
+        imgd: "#",
+        details: [
+            "Relevant courses: Data Structures, OS, DBMS, Web Dev",
+        ],
+    },
+    {
+        id: 2,
+        institution: "Pancharul Srihari Vidyamandir",
+        deg: "11th Standard - 12th Standard",
+        branch: "Science (Phy, Chem, Math, Bio)",
+        year: "2022-2024",
+        imgd: "#",
+    },
+    {
+        id: 3,
+        institution: "Pancharul Srihari Vidyamandir",
+        deg: "5th Standard - 10th Standard",
+        branch: "Schooling (Phy, Math, Geo, His, Bio)",
+        year: "2016-2022",
+        imgd: "#",
+    },
+];
+
+export default educations;
