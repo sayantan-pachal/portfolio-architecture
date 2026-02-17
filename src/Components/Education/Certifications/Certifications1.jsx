61ef620455f3b54c85c77072e28761b0848261fc
Sayantan Pachal <sayantanpachal006@gmail.com>
2026-02-17 18:15:37 +0530

    update

--- src/Components/Education/Certifications/Certifications1.jsx
+++ src/Components/Education/Certifications/Certifications1.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 function Certifications1() {
   return (
+    // Left side of the Certifications section, showing certifications icon left side of the page...
+    // Showing additional data of the certifications on right side of the page...
     <div>Certifications1</div>
   )
 }
---
+++ src/Components/Education/Certifications/Certifications2.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Certifications2() {
+  return (
+    // Right side of the Certifications section, showing certifications icon right side of the page...
+    // Showing additional data of the certifications on left side of the page...
+    <div>Certifications2</div> 
+  )
+}
+
+export default Certifications2
