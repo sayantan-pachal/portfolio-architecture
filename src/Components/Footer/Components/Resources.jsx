b220d641fbbfd35ed36346381a0e413fbd999003
Sayantan Pachal <sayantanpachal006@gmail.com>
2026-02-17 18:21:41 +0530

    update

---
+++ src/Components/Footer/Components/Resources.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function Resources() {
+  return (
+    // Resources section, showing various resources(Navigation) related to the website's content...
+    <div>Resources</div>
+  )
+}
+
+export default Resources
---
+++ src/Components/Footer/Components/Social.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function Social() {
+  return (
+    // Social section, showing various social media links and icons related to the website's content...
+    <div>Social</div>
+  )
+}
+
+export default Social
