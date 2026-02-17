4c5cc03474b0d1d75fa6e23901c3f24be944714c
Sayantan Pachal <sayantanpachal006@gmail.com>
2026-02-17 18:22:49 +0530

    update

---
+++ src/Components/Home/Parts/HomeIntro.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function HomeIntro() {
+  return (
+    // HomeIntro section, showing introductory content related to the website's theme...
+    <div>HomeIntro</div>
+  )
+}
+
+export default HomeIntro
